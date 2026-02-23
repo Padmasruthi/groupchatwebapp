ae14d8bfc94398ed5ff1774e6841dc9af4e5dc99
Sruthi <30sruthihariharan2002@gmail.com>
2026-02-23 12:18:35 +0530

    Updated MongoDB connection

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,8 +2,8 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb://127.0.0.1:27017/groupchat");
-    console.log("MongoDB Connected");
+mongoose.connect(process.env.MONGO_URI)   
+ console.log("MongoDB Connected");
   } catch (error) {
     console.error("DB Error:", error.message);
     process.exit(1);
